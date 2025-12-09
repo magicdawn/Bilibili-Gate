a452b5a7cc70d62c5508057b2f69245bf8bcc79d
magicdawn <magicdawn@qq.com>
2025-12-10 00:06:14 +0800

    deps: upgrade @magicdawn/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,4 @@ import { fromSxzz } from '@magicdawn/eslint-config'
 
 export default fromSxzz().overrideRules({
   'unocss/order': ['warn', { unoFunctions: ['clsx', 'unoMerge', 'useUnoMerge'] }],
-  'unicorn/no-array-sort': 'off',
-  'unicorn/no-array-reverse': 'off',
-  'unicorn/prefer-global-this': 'off',
-  'unicorn/no-useless-undefined': 'off', // 关键这条规则太蠢... 返回值期望 string|undefined, `return` 会认为是 void
 })
