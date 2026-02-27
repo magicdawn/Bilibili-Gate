383db60b85047c5ad2e5239878e1ccedae2e7607
magicdawn <magicdawn@qq.com>
2026-02-27 23:27:17 +0800

    chore: use unsafeWindow

--- src/modules/rec-services/pc-initial-rec.ts
+++ src/modules/rec-services/pc-initial-rec.ts
@@ -6,7 +6,16 @@ const debug = baseDebug.extend('modules:rec-services:pc-initial-rec')
 
 export async function getWebInitialRecommendItems(abortSignal: AbortSignal): Promise<PcRecItem[]> {
   // wait `window.__pinia`, then use `feed.data.recommend.item`
-  const __pinia = await poll(() => (globalThis as any).__pinia, { interval: 100, timeout: 1_000, abortSignal })
+  const __pinia = await poll(
+    () => {
+      try {
+        return (unsafeWindow as any).__pinia
+      } catch {
+        return
+      }
+    },
+    { interval: 100, timeout: 1_000, abortSignal },
+  )
   const items: PcRecItem[] = __pinia?.feed?.data?.recommend?.item || []
   debug('initial rec items: %o', items)
   return items
