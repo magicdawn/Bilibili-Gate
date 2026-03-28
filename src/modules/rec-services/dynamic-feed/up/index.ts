7518f106c1e30c6b165cdaa70554a70d25c1cf8a
magicdawn <magicdawn@qq.com>
2026-03-28 10:06:37 +0800

    fix(dynamic-feed): handle web-dynamic/v1/portal API update

--- src/modules/rec-services/dynamic-feed/up/index.ts
+++ src/modules/rec-services/dynamic-feed/up/index.ts
@@ -3,14 +3,21 @@
  */
 
 import { request } from '$request'
-import type { DynamicPortalJSON } from './portal-types'
+import type { DynamicPortalJSON, DynamicPortalUp } from './portal-types'
 
 /**
  * 最近有更新的 UP
+ *
+ * 2026-03: `up_list_more` 参数会影响 up_list 结构
  */
 export async function getRecentUpdateUpList() {
-  const res = await request.get('/x/polymer/web-dynamic/v1/portal')
+  const res = await request.get('/x/polymer/web-dynamic/v1/portal', {
+    params: { up_list_more: 0 },
+  })
   const json = res.data as DynamicPortalJSON
-  const list = json?.data?.up_list || []
-  return list
+  const rawUpList = (json?.data?.up_list || []) as
+    | DynamicPortalUp[]
+    | { has_more: boolean; items: DynamicPortalUp[]; offset: string }
+  const arr = Array.isArray(rawUpList) ? rawUpList : rawUpList.items
+  return arr
 }
