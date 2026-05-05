6c5b2746c13a04abd7d5c78bd47390ab38fe5cea
magicdawn <magicdawn@qq.com>
2026-05-06 00:28:00 +0800

    deps: try oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: true,
+  htmlWhitespaceSensitivity: 'css',
+  sortPackageJson: false,
+})
